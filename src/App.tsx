742c401fb9d9ba158ab2f20aebab2af1ccc50e46
StefanVuko <114865138+StefanVuko@users.noreply.github.com>
2025-01-17 23:43:16 +0100

    More Basic Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <>
-      <p>Hello World </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router";
+
+function NotFound() {
+  return <Navigate to="/" />;
+}
+
+export default NotFound;
